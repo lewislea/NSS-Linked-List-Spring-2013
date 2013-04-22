7e6f173f731330c0e97381d5c8147ca50a8a1fe3
lewislea <wlewislea@gmail.com>
2013-04-22 15:23:44 -0500

    refactored and peer reviewed

--- lib/linked_list_item.rb
+++ lib/linked_list_item.rb
@@ -4,27 +4,16 @@ class LinkedListItem
 
   def initialize(payload)
   	@payload = payload
-  	
-  	
   end
 
   def next_list_item= (linked_list_item)
   	if linked_list_item == self
   		raise(ArgumentError, "can't make self next list item")
   	end
-
   	@next_list_item = linked_list_item
-  	
   end
 
   def last?
-  	next_list_item.nil? 
-
-    # if next_list_item.nil?
-   #    true
-   #  else
-   #    false
-   #  end
-
+  	next_list_item.nil?
   end
 end
