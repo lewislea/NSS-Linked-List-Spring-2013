c4a1df5c4d0ab0c6ba131ccf66253fc7ecc7a59c
lewislea <wlewislea@gmail.com>
2013-04-22 12:08:29 -0500

    finished linked lists thru bonus tests. still need to refactor

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -6,19 +6,79 @@ class LinkedList
   def initialize *args
   end
 
-  def add_item(payload)
+  def add_item(payload_for_new_list_item)
+    
+    next_item = LinkedListItem.new(payload_for_new_list_item
+      )
+   if @first_item.nil?
+   @first_item = next_item
+   else
+      final_item = @first_item
+      while !final_item.last?
+        final_item = final_item.next_list_item
+      end
+    final_item.next_list_item = next_item    
+   end
+   
+
   end
 
   def get(n)
+
+    item = @first_item
+
+    n.times do 
+      raise IndexError, "there are no list items" if item.nil?
+      item = item.next_list_item
+    end
+
+    item.payload
+
   end
 
   def last
+    
+    return nil if @first_item.nil? 
+
+    i = 0
+    item = @first_item
+  
+    until item.next_list_item == false
+      i += 1
+      item = i
+    end
+
+    item.payload
+
   end
 
   def size
+    size = 0
+
+    item = @first_item
+
+    until item.nil?
+    size += 1
+    item = item.next_list_item
+    end
+    size
   end
 
   def to_s
+   
+    item = @first_item
+    
+    if item.nil?
+      linked_list = "| |"
+    else
+      payload_list = item.payload
+      until item.last?
+        item = item.next_list_item
+        payload_list += ", " + item.payload 
+      end
+      linked_list = "| #{payload_list} |"    
+    end
+    
   end
 
   # ========= Bonus ========== #
--- lib/linked_list_item.rb
+++ lib/linked_list_item.rb
@@ -2,12 +2,29 @@ class LinkedListItem
   attr_reader :next_list_item
   attr_reader :payload
 
-  def initialize( payload )
+  def initialize(payload)
+  	@payload = payload
+  	
+  	
   end
 
   def next_list_item= (linked_list_item)
+  	if linked_list_item == self
+  		raise(ArgumentError, "can't make self next list item")
+  	end
+
+  	@next_list_item = linked_list_item
+  	
   end
 
   def last?
+  	next_list_item.nil? 
+
+    # if next_list_item.nil?
+   #    true
+   #  else
+   #    false
+   #  end
+
   end
 end
