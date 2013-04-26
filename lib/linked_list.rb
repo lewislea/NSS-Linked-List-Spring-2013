49977b92a44d22bf542546f1d351ea180a961551
lewislea <wlewislea@gmail.com>
2013-04-26 07:18:36 -0500

    first steps of implementing sorting

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -4,18 +4,19 @@ class LinkedList
   attr_reader :first_item
 
   def initialize *args
+    args.each {|args| add_item(args)}
   end
 
   def add_item(payload)
-  next_item = LinkedListItem.new(payload)
+  new_item = LinkedListItem.new(payload)
   if @first_item.nil?
-    @first_item = next_item
+    @first_item = new_item
   else
     final_item = @first_item
-    while !final_item.last?
+    until final_item.last?
       final_item = final_item.next_list_item
     end
-    final_item.next_list_item = next_item
+    final_item.next_list_item = new_item
   end
   end
 
@@ -65,14 +66,39 @@ class LinkedList
     item = @first_item
     return nil if item.nil?
     i = 0
-      until payload == item.payload
-        i += 1
-        item = item.next_list_item
-        return nil if item.nil?
-      end
+    until payload == item.payload
+      i += 1
+      item = item.next_list_item
+      return nil if item.nil?
+    end
     i
+    # use break to eliminate duplicate nil lines?
 
   end
+
+  def sorted?
+
+    return true if @first_item.nil?
+
+    item = @first_item
+    return true if item.next_list_item.nil?
+
+    until item.last?
+      if item < item.next_list_item
+        item = item.next_list_item
+      else
+        return false
+      end
+
+  end
+
+  def sort
+    if @first_item.nil?
+      "| |"
+  end
+
+
+  # until sorted?
 
   # ========= Bonus ========== #
 
--- lib/linked_list_item.rb
+++ lib/linked_list_item.rb
@@ -1,4 +1,5 @@
 class LinkedListItem
+  include Comparable
   attr_reader :next_list_item
   attr_reader :payload
 
@@ -16,4 +17,35 @@ class LinkedListItem
   def last?
     next_list_item.nil?
   end
-end
+
+  def === other_item
+    self.object_id == other_item.object_id
+  end
+
+  def <=> other
+    # Comparators return -1, 0, or 1 to indicate > , ==, or <
+    # Precedence: integers, then strings, then symbols (each sorted
+    # individually)
+    if self.payload.class == other.payload.class
+      # I implemented this basic comparison for you.
+      return self.payload <=> other.payload
+    end
+    # You will have to implement the logic for what to do to compare types that aren't equal.
+    0
+    if self.payload.class == Symbol
+    return 1
+    end
+    if self.payload.class == Fixnum
+    return -1
+    end
+    if self.payload.class == String and other.payload.class == Symbol
+    return -1
+    end
+    if self.payload.class == String and other.payload.class == Fixnum
+    return 1
+    end
+
+  end
+
+
+end #class end
